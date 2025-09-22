4e47b4505ab378c49a393da6a9c0cf6dc4ace439
lauri <lauri.vosmi@voco.ee>
2025-09-22 15:21:53 +0300

    More functions

--- 1_class_and_object/person.js
+++ 1_class_and_object/person.js
@@ -1,3 +1,91 @@
 class Person {
+    #age = 0;
+    #weight = 0;
+    #height = 0;
+    #name = "";
+
+    constructor(name, weight = 0, height = 0) {
+        this.#name = name;
+        this.setWeight(weight);
+        this.setHeight(height);
+    }
+
+    becomeHeavier() {
+        return ++this.#weight;
+    }
+
+    becomeTaller() {
+        return ++this.#height;
+    }
+
+    becomeOlder() {
+        this.#age++;
+    }
+
+    printPerson() {
+        const bmiVal = this.getBMI();
+        const bmiStr = bmiVal !== null ? bmiVal.toFixed(2) : "n/a";
+        console.log(`name: ${this.#name}, age: ${this.#age}, weight: ${this.#weight} kg, height: ${this.#height} m, bmi: ${bmiStr}`);
+    }
+
+    isAdult() {
+        return this.#age >= 18;
+    }
+
+    setAge(age) {
+        this.#age = age;
+    }
+
+    getAge() {
+        return this.#age;
+    }
+
+    setName(name) {
+        this.#name = name;
+    }
+
+    getName() {
+        return this.#name;
+    }
+
+    setWeight(weight) {
+        if (weight > 0) {
+            this.#weight = weight;
+        } else if (weight !== 0) {
+            console.error("Weight must be greater than 0 (kg)");
+        }
+    }
+
+    getWeight() {
+        return this.#weight;
+    }
+
+    setHeight(height) {
+        if (height > 0) {
+            this.#height = height;
+        } else if (height !== 0) {
+            console.error("Height must be greater than 0 (m)");
+        }
+    }
+
+    getHeight() {
+        return this.#height;
+    }
+
+    getBMI() {
+        if (this.#weight > 0 && this.#height > 0) {
+            const bmi = this.#weight / (this.#height * this.#height);
+            return Number(bmi.toFixed(2));
+        } else {
+            console.error("Weight and height must be set and greater than 0 to calculate BMI");
+            return null;
+        }
+    }
+
     
+    bmi() {
+        return this.getBMI();
+    }
 }
+
+module.exports = Person
--- 1_class_and_object/persons.js
+++ 1_class_and_object/persons.js
@@ -1,3 +1,37 @@
-const bob = newPerson
+const Person = require("./person.js")
+const bob  = new Person("Bob")
+const brian  = new Person("Brian")
+const alice  = new Person("Alice")
 
-console.log("Hello");
+
+bob.setWeight(70)
+bob.setHeight(1.75)
+
+brian.setWeight(80)
+brian.setHeight(1.80)
+
+alice.setWeight(60)
+alice.setHeight(1.65)
+
+const persons = [bob, brian, alice]
+
+while (bob.getAge() < 15) {
+    bob.becomeOlder()
+}
+
+for (let i = 0; i < 3; i++) {
+    brian.becomeOlder()
+}
+
+
+persons.forEach(person => {
+    if (person.isAdult()) {
+        console.log(`${person.getName()} is adult`)
+    } else {
+        console.log(`${person.getName()} is not adult`)
+    }
+})
+
+bob.printPerson()
+brian.printPerson()
+alice.printPerson()
